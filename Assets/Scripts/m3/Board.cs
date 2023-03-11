543221463d87675ab8b73ac946a42c134369e26d
Sergei Blockiy <sergei.blockiy@gmail.com>
2023-03-11 16:31:08 +0200

    refactor

--- Assets/Scripts/m3/Board.cs
+++ Assets/Scripts/m3/Board.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DG.Tweening;
-using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
