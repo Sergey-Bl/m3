f3bfe6a2e7e9f2c1a1c667f88f73976235680eee
Sergei Blockiy <sergei.blockiy@gmail.com>
2023-03-11 14:29:26 +0200

    AddCommitWithGame

---
+++ Assets/Scripts/Row.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace match3
+{
+    public sealed class Row : MonoBehaviour
+    {
+        public Tile[] tiles;
+    }
+}
---
+++ Assets/Scripts/TileTypeAsset.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace match3
+{
+    [CreateAssetMenu(menuName = "Match 3 Engine/Tile Type Asset")]
+    public sealed class TileTypeAsset : ScriptableObject
+    {
+        public int id;
+
+        public int value;
+
+        public Sprite sprite;
+    }
+}
